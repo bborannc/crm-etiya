f29ca3a27983a4ec910d336cf32c7a90f2b4b7cd
Boran <borancumaklar@icloud.com>
2025-08-05 13:29:48 +0300

    feat: proje dizini oluşturuldu

---
+++ src/main/java/com/boran/crm/domain/entity/User.java
@@ -0,0 +1,29 @@
+package com.boran.crm.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name= "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique =true)
+    private String email;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name="user_roles",joinColumns = @JoinColumn(name="user_id"))
+    @Column(name="role")
+    private Set<String> roles;
+
+}
